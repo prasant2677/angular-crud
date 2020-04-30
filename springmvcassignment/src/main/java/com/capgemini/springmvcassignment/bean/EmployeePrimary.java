d476405fecacdf0f2434aadcf66eba38945c6bcb
prasant2677 <61830386+prasant2677@users.noreply.github.com>
2020-04-30 08:56:58 +0530

    Add files via upload

---
+++ springmvcassignment/src/main/java/com/capgemini/springmvcassignment/bean/EmployeePrimary.java
@@ -0,0 +1,69 @@
+package com.capgemini.springmvcassignment.bean;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="employee_primary")
+public class EmployeePrimary implements Serializable{
+	@Id
+	@Column
+	private int id;
+	@Column
+	private String name;
+	@Column
+	private int age;
+	@Column
+	private double salary;
+	@Column
+	private String designation;
+	@Column
+	private String password;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	public String getDesignation() {
+		return designation;
+	}
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+}
